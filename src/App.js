f72e0d15a77168a21f195d4b37206423335c3290
Batnaidan Tsend <batnaidan@gmail.com>
2021-01-09 19:00:51 +0800

    Structuring

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import './components/Header';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Header></Header>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+// import './Header.css';
+
+export default class Header extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>About SisiSchedule</li>
+          <li>Home</li>
+        </ul>
+      </div>
+    );
+  }
+}
