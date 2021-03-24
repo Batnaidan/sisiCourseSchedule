f41e39391a8f92dd7095a607b87ea26f643ff605
Batnaidan Tsend <batnaidan@gmail.com>
2021-03-24 19:07:37 +0800

    Fixed TreeView

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,22 +1,18 @@
 import React, { Component } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import TreeView from '@material-ui/lab/TreeView';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import TreeItem from '@material-ui/lab/TreeItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
 // import exitButtonImg from '../images/exitButton.png';
 import Rodal from 'rodal';
 import 'rodal/lib/rodal.css';
 import api from '../api';
 import './Modal.css';
-
-const useStyles = makeStyles({
-  root: {
-    height: 216,
-    flexGrow: 1,
-    maxWidth: 300,
-  },
-});
+import RenderCourse from './RenderCourse';
 
 export default class Modal extends Component {
   constructor(props) {
@@ -24,9 +20,11 @@ export default class Modal extends Component {
     this.state = {
       isLoading: true,
       subSchool: null,
+      didClick: false,
+      courses: null,
+      selectedCourses: null,
     };
   }
-  classes = useStyles();
 
   async componentDidMount() {
     let data = (await api.getDep()).data.data;
@@ -39,9 +37,16 @@ export default class Modal extends Component {
 
   insertCourse = () => {
     console.log(this.data);
-    // this.props.chosenCourses.push('selected shit');
+    this.props.chosenCourses.push(this.state.selectedCourses);
   };
-
+  renderCourse = async (depId) => {
+    let data = (await api.getAllCourses(depId)).data.data;
+    this.setState({
+      didClick: true,
+      courses: data,
+    });
+    console.log(data);
+  };
   render() {
     return (
       <Rodal
@@ -55,6 +60,31 @@ export default class Modal extends Component {
         {this.state.isLoading ? (
           'Loading'
         ) : (
+          // <div>
+          //   <FormControl style={{ minWidth: 300, maxWidth: 300 }}>
+          //     <InputLabel htmlFor="SubSchool">SubSchool</InputLabel>
+          //     <Select defaultValue="" id="SubSchool">
+          //       <MenuItem value="">
+          //         <em>None</em>
+          //       </MenuItem>
+          //       {this.state.subSchool.map((el, index) => (
+          //         <MenuItem value={index}>{el.Namem}</MenuItem>
+          //       ))}
+          //     </Select>
+          //     {/* <button onClick={this.insertCourse}>Select</button> */}
+          //   </FormControl>
+
+          //   <div>
+          //     <FormControl style={{ minWidth: 300 }}>
+          //       <InputLabel htmlFor="Department">Department</InputLabel>
+          //       <Select defaultValue="" id="Department" autoWidth>
+          //         <MenuItem value="">
+          //           <em>None</em>
+          //         </MenuItem>
+          //       </Select>
+          //     </FormControl>
+          //   </div>
+          // </div>
           <TreeView
             className="root"
             defaultCollapseIcon={<ExpandMoreIcon />}
@@ -63,13 +93,35 @@ export default class Modal extends Component {
             {this.state.subSchool.map((el, index) => (
               <TreeItem nodeId={`${index}`} label={el.Namem}>
                 {el.dep.map((dep) => (
-                  <TreeItem nodeId={dep.ID} label={dep.Namem}></TreeItem>
+                  <TreeItem
+                    nodeId={dep.ID}
+                    label={dep.Namem}
+                    onClick={() => this.renderCourse(dep.ID)}
+                  ></TreeItem>
                 ))}
               </TreeItem>
             ))}
           </TreeView>
         )}
-        <button onClick={this.insertCourse}>Select</button>
+        {this.state.didClick ? (
+          <FormControl style={{ minWidth: 300 }}>
+            <InputLabel htmlFor="Department">Department</InputLabel>
+            <Select defaultValue="" id="Department" autoWidth>
+              {this.state.courses.map((el, index) => (
+                <MenuItem value={index}>{el.nm}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        ) : (
+          <FormControl style={{ minWidth: 300 }}>
+            <InputLabel htmlFor="Department">Department</InputLabel>
+            <Select defaultValue="" id="Department" autoWidth>
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+            </Select>
+          </FormControl>
+        )}
       </Rodal>
     );
   }
